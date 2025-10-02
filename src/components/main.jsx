f4ecf5de4affa28eafadcb5b0f4531848c3c01f1
Anxhelika <krecianxhelika04@gmail.com>
2025-10-02 18:32:57 +0200

    krijimik i komponentit app dhe main plus nje stilizim i vogel pe rte krijuar nje border

---
+++ src/components/main.jsx
@@ -0,0 +1,11 @@
+export default function Main(){
+  return(
+    <>
+        <div className="frame">
+            <div className="content">
+                <h1>hello there</h1>
+            </div>
+        </div>
+    </>
+  )
+}
