0a7a236ebf9348f5fa82258793d0f65604ada9b0
Anxhelika <krecianxhelika04@gmail.com>
2025-10-02 20:28:22 +0200

    Shtimib i funksionalitetit qe dices te ken nje numer random

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,13 +1,25 @@
 import Dice from "./Dice"
+import React from "react"
 
 export default function Main(){
+
+    const [dice, setDice] = React.useState(allNewDice())
+    function allNewDice(){
+        return new Array(10)
+                       .fill(0)
+                       .map(num => Math.ceil(Math.random()*6))
+    }
+    function diceElements(){
+        return  dice.map(num => 
+       <Dice value={num} />
+    )
+}
   return(
     <>
         <div className="frame">
             <div className="content">
                 <div className="dice-container">
-               <Dice value={1}/><Dice value={1}/><Dice value={1}/><Dice value={1}/><Dice value={1}/>
-               <Dice value={1}/><Dice value={1}/><Dice value={1}/><Dice value={1}/><Dice value={1}/>
+                    {diceElements()}
                </div>
 
                 </div> 
