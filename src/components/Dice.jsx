45dcbae656153a66fc7909e071e75caabd8929cc
Anxhelika <krecianxhelika04@gmail.com>
2025-10-04 15:13:15 +0200

    zevendesimi i numrave me pika sic jan tek dice dhe rregullimi tyre per ti bere responsive

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,4 +1,13 @@
 export default function Dice(props){
+    //kordinatat ne varesi te nr te zarit si do vendosen pikat
+    const pipPositions = {
+        1:[[2,2]],
+        2:[[1,1],[3,3]],
+        3:[[1,1],[2,2],[3,3]],
+        4:[[1,1],[1,3],[3,1],[3,3]],
+        5:[[1,1],[1,3],[2,2],[3,1],[3,3]],
+        6:[[1,1],[2,1],[3,1],[1,3],[2,3],[3,3]]
+    }
   return(
     <>
         <button onClick={props.holdDice} 
@@ -9,10 +18,13 @@ export default function Dice(props){
                     onMouseMove={props.onMouseMove}
                     onMouseLeave={props.onMouseLeave}
                     style={props.style}
-                    
                     >
-            {props.value}
+                        <div className="pip-grid">
+                            {pipPositions[props.value].map(([row, col], index)=>(
+                                <span key={index} className={`pip row-${row} col-${col}`}></span>
+                            ))}
 
+                        </div>
         </button>
     </>
   )
